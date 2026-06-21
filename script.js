e489af59e548ee23cdfccfd287b0e7a8ccf244c5
Murilo Leandro <muriloleandro188@gmail.com>
2026-06-21 15:09:51 -0300

    fix pages

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function playAudio(id) {
+function playAudio1(id) {
   const audio = document.getElementById("audio" + id);
-  audio.currentTime = 0;
+  audio.currentTime = 145;
+  audio.play();
+}
+
+function playAudio2(id) {
+  const audio = document.getElementById("audio" + id);
+  audio.currentTime = 80;
+  audio.play();
+}
+
+function playAudio3(id) {
+  const audio = document.getElementById("audio" + id);
+  audio.currentTime = 110;
   audio.play();
 }
 
